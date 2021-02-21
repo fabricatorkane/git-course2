4b2a6f4ff1df02597ce91d389853633e346371b6
fabricatorkane <melkorsau@gmail.com>
2021-02-22 00:02:41 +0300

    js

---
+++ app.js
@@ -0,0 +1,12 @@
+const firstName = "Naum"
+const lastName='Tarnog'
+//name='Mar'
+
+console.log('Имя человека: ' + firstName + ', а фамилия: ' + lastName)
+//alert('Имя человека: ' + firstName + ', а фамилия: ' + lastName)
+//const Age = prompt('Введите что-нибудь')
+//alert(Age + ' ' + firstName)
+
+const currentYear=2021
+const birthYear=1993
+const age = currentYear - birthYear
