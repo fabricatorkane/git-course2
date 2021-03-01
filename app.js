11027b4f3f57dc14427936483bd3cf39adb8d214
fabricatorkane <melkorsau@gmail.com>
2021-03-01 23:18:15 +0300

    somejs4

--- app.js
+++ app.js
@@ -19,3 +19,16 @@ console.log(hiJack);
 for (let i = 1; i <= 5; i = i + 1) {
     console.log("i = " + i);
   };
+
+  function printText() {                   //Определение функции
+    document.write("Hello, World!");        //Тело функции - вывод текста в документ
+  };
+
+  printText();
+  var printText = function(a) {document.write(a);}("\n Hello World!");
+  var printText = function(a) {document.write(a);}(" Тадааам");
+
+  let age_1 = prompt('Сколько тебе лет?', 100); //задаем вопрос и дефолтное значение (100) в поле для ввода
+  let areYouSure = confirm("Уверен?");
+  alert(`Тебе ${age_1} лет!`); // Тебе 100 лет! - выводятся данные
+  document.write(` Тебе ${age_1} лет! Неудачник!`)
