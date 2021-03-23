3cd66501055331e56b30dbbd2cde824285ba383d
fabricatorkane <melkorsau@gmail.com>
2021-03-24 01:09:08 +0300

    morejs7

--- js/trainy.js
+++ js/trainy.js
@@ -13,6 +13,12 @@ for (var inY = 0; inY <= b.length-1; inY= inY + 1) {
         y = y + b[inY];
     }
 
+console.log(x + y);
 
-
-console.log(x + y)
+let aB = 'abcd';
+let bC = '123456'
+console.log(aB.concat(bC)); // объединил 2 строки 
+console.log(aB.concat(bC).split('')); // разбили строки на массивы
+console.log(aB.concat(bC).split('').reverse()); // отзеркалили элементы массива
+console.log(aB.concat(bC).split('').reverse().join('')); //объединили массивы в строку
+console.log(aB.concat(bC).split('').reverse().join('').concat('Иванов')); // добавили к массиву "Иванов"
